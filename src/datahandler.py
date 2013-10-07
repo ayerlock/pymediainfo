d8b5e17a61b4886e6096e130f437c3e572e76e3d
Michael J. Ayers <ayersmj@redhat.com>
2013-10-07 16:11:01 -0400

    --	added datahandler to handle file output writing

---
+++ src/datahandler.py
@@ -0,0 +1,56 @@
+###------------------------------------------------------------------------------------------------------------------------------
+class DataHandler():
+	def __init__( self, filename ):
+		self.data									= []
+		self.filename								= filename
+		if ( os.path.isfile( self.filename ) ):
+			self.ReadData()
+	#----------------------------------------------------------------------------------------------------------------------
+	def OpenFile( self ):
+		import codecs
+		try:
+			if ( os.path.isfile( self.filename ) ):
+				self.WriteHDL						= codecs.open( self.filename, mode="a", encoding="utf-8" )
+			else:
+				self.WriteHDL						= codecs.open( self.filename, mode="w", encoding="utf-8" )
+		except:
+			print( "Error opening %s for writing!" % self.filename )
+	#----------------------------------------------------------------------------------------------------------------------
+	def CloseFile( self ):
+			self.WriteHDL.close()
+	#----------------------------------------------------------------------------------------------------------------------
+	def Append( self, data ):
+		try:
+			WriteHDL								= codecs.open( self.filename, mode="a", encoding="utf-8" )
+		except:
+			print( "Error writing to file %s!" % self.filename )
+		else:
+			try:
+				WriteHDL.write( '%s\n' % ( data ) )
+			except Exception, e:
+				print( "Error writing to file %s.\tActual: %s" % ( self.filename, e ) )
+			WriteHDL.close()
+	#----------------------------------------------------------------------------------------------------------------------
+	def Read( self ):
+		ReadHDL										= codecs.open( self.filename, mode="r", encoding="utf-8" )
+		for line in ReadHDL.readlines():
+			line									= line.rstrip()
+			self.data.append( line )
+		self.data									= list(set( self.data ))
+		ReadHDL.close()
+	#----------------------------------------------------------------------------------------------------------------------
+	def Write( self, data ):
+		try:
+			WriteHDL								= codecs.open( self.filename, mode="w", encoding="utf-8" )
+		except:
+			print( "Error opening %s for writing!" % self.filename )
+		else:
+			try:
+				WriteHDL.write( '%s\n' % ( data ) )
+			except Exception, e:
+				print( "Error writing to file %s.\tActual: %s" % ( self.filename, e ) )
+			WriteHDL.close()
+	#----------------------------------------------------------------------------------------------------------------------
+	def data( self ):	
+		return self.data
+		#dataHandle										= DataHandler( file )
--- src/pymedia.py
+++ src/pymedia.py
@@ -1,10 +1,12 @@
 #!/bin/python
 
-import	ctypes
+import	codecs
+import	datahandler
 import	logging
 import	os
 import	re
 import	sys
+from	bs4		import NavigableString, BeautifulSoup	as bSoup
 
 sys.setrecursionlimit(5000)
 ###------------------------------------------------------------------------------------------------------------------------------
@@ -12,22 +14,24 @@ def argparser():
 	import argparse
 	
 	global Logger
-	ap = argparse.ArgumentParser( description = 'A pythonized front end to MediaInfo.', prog = os.path.basename(re.sub(".py", "", sys.argv[0])) )
+	ap = argparse.ArgumentParser( description = 'A pythonized front end to MediaInfo.', prog = os.path.basename(re.sub("\.py$", "", sys.argv[0])) )
 	gap = ap.add_argument_group( 'program information' )
 	gap.add_argument( 		'--version',	action = 'version',		version = '%(prog)s 0.1' )
-	gap.add_argument(		'--program',	action = 'store',		dest = "prog",		metavar = "prog",			default = os.path.basename(re.sub(".py", "", sys.argv[0])))
+	gap.add_argument(		'--program',	action = 'store',		dest = "prog",		metavar = "prog",			default = os.path.basename(re.sub("\.py#", "", sys.argv[0])))
 	gap = ap.add_argument_group( 'standard functionality' )
 	gap.add_argument(		'--dryrun',		action = 'store_true' )
 	gap.add_argument(		'--force',		action = 'store_true' )
-	gap.add_argument( '-v',	'--verbose',	action = 'count',		default = 1 )
 	gap.add_argument( '-f',	'--file',		action = 'store',		dest = "file",		metavar = "file" )
+	gap.add_argument( '-i',	'--info',		action = 'store_true' )
+	gap.add_argument( '-v',	'--verbose',	action = 'count',		default = 1 )
 	gap = ap.add_argument_group( 'logging' )
 	gap.add_argument( 		'--loglevel',	action = 'store',		dest = "loglevel",	metavar = "logging level",	default = 'info',	choices = ['crit', 'error', 'warn', 'notice', 'info', 'verbose', 'debug', 'insane'] )
 	gap.add_argument(		'--logfile',	action = 'store',		dest = "logfile",	metavar = "logfile" )
 	gap = ap.add_argument_group( 'output options' )
 	gap.add_argument(		'--pprint',		action = 'store_true' )
 	gap.add_argument(		'--save',		action = 'store_true' )
-	gap.add_argument( '-o',	'--outfile',	action = 'store',		dest = "outfile",	metavar = "outfile",		default = ( '%s.html' % (os.path.basename(re.sub(".py", "", sys.argv[0])))) )
+	gap.add_argument( 		'--format',		action = 'store',		dest = "format",	metavar = "format",			default = "normal",	choices = ['html', 'normal', 'xml'] )
+	gap.add_argument( '-o',	'--outfile',	action = 'store',		dest = "outfile",	metavar = "outfile",		default = ( '%s' % (os.path.basename(re.sub("\.py$", "", sys.argv[0])))) )
 	
 	ProgArgs								= ap.parse_args()
 	Logger									= initLog( ProgArgs.loglevel, ProgArgs.logfile )
@@ -65,6 +69,7 @@ def initLog( LLevel, LogFile = None, module = None ):
 		LogFHandler.setFormatter( logging.Formatter( LogFormat, datefmt='%I:%M:%S %p' ) )
 		initLogger.addHandler( LogFHandler )
 	return initLogger
+
 ###------------------------------------------------------------------------------------------------------------------------------
 def LibImport( libPath, libFile ):
 	if ( not os.path.isdir( libPath ) ):
@@ -93,6 +98,65 @@ def pStatus( status ):
 		sys.exit( status )
 	return True
 ###------------------------------------------------------------------------------------------------------------------------------
+def OpenFile( file ):
+	MediaInfo.Open( file )
+	return True
+###------------------------------------------------------------------------------------------------------------------------------
+def CloseFile():
+	MediaInfo.Close() 
+	return True
+###------------------------------------------------------------------------------------------------------------------------------
+def GetInfo( file, type ):
+	if ( OpenFile( file ) ):
+		if ( type == "html" ):
+			MediaInfo.Option( "Inform", "HTML" )
+		elif ( type == "xml" ):
+			MediaInfo.Option( "Inform", "XML" )
+		MediaInfo.Option( "Complete", "1" )
+		InformData									= MediaInfo.Inform()
+		CloseFile()
+	else:
+		InformData									= False
+	return InformData
+###------------------------------------------------------------------------------------------------------------------------------
+def Info( ProgArgs ):
+	HTMLOut											= False
+	XMLOut											= False
+	
+	if ( ProgArgs.format == "html" ):
+		HTMLOut										= True
+		InformData									= GetInfo( ProgArgs.file, "html" )
+		outExt										= "html"
+	elif ( ProgArgs.format == "xml" ):
+		XMLOut										= True
+		InformData									= GetInfo( ProgArgs.file, "xml" )
+		outExt										= "xml"
+	else:
+		InformData									= GetInfo( ProgArgs.file, "normal" )
+		outExt										= "out"
+		
+	if ( InformData == False ):
+		Logger.error( "Error getting media information from file.")
+	else:
+		if ( ProgArgs.pprint ):
+			if ( HTMLOut ):
+				InformSoup								= bSoup( InformData, "html5lib" )
+			elif ( XMLOut ):
+				InformSoup								= bSoup( InformData, "xml" )
+			InformData								= InformSoup.prettify()
+		
+		if ( ProgArgs.outfile ):
+			curDir									= os.getcwd()
+			outfile									= ( "%s/%s.%s" % ( curDir, ProgArgs.outfile, outExt ) )
+			OutHandle								= DataHandler( file )
+			OutHandle.Write( InformData )
+		else:
+			print( InformData )
+		
+	print( "XML Output:\t%s" % XMLOut )
+	print( "HTML Output:\t%s" % HTMLOut )
+	return True
+###------------------------------------------------------------------------------------------------------------------------------
 def main():
 	ProgArgs										= argparser()
 	if ( not ProgArgs ):
@@ -101,11 +165,15 @@ def main():
 		pStatus( 2 )
 		
 	###--- Import MediaInfoDLL ------------------------------------------------------------------------
+	global	MediaInfo
 	PyMediaInfoPath									= "/usr/include/MediaInfoDLL"
 	PyMediaInfoLibFile								= "/usr/include/MediaInfoDLL/MediaInfoDLL.py"
-	PyMedia											= LibImport( PyMediaInfoPath, PyMediaInfoLibFile )
-	###------------------------------------------------------------------------------------------------
-		
+	PM												= LibImport( PyMediaInfoPath, PyMediaInfoLibFile )
+	MediaInfo										= PM.MediaInfo()
+	
+	if ( ProgArgs.info ):
+		success										= Info( ProgArgs )
+	
 	return pStatus( 0 )
 ###------------------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
